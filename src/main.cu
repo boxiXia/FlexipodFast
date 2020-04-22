9822a8d2a8d2d9683e708f87b085767b17d14925
DonovanZhu <49983377+DonovanZhu@users.noreply.github.com>
2020-04-22 10:15:48 -0400

    Update main.cu

--- src/main.cu
+++ src/main.cu
@@ -9,13 +9,19 @@
 #define CUDA_DEVICE
 #endif
 
-#include<cuda_runtime.h>
-#include<cuda_device_runtime_api.h>
-#include<device_launch_parameters.h>
+#include <cuda_runtime.h>
+#include <cuda_device_runtime_api.h>
+#include <device_launch_parameters.h>
 
 #include <thrust/device_vector.h>
-#include<thrust/system/cuda/experimental/pinned_allocator.h>
+#include <thrust/system/cuda/experimental/pinned_allocator.h>
 
+#include <stdio.h>
+#include <sstream>
+#include <fstream>
+#include <iostream>
+#include <stdlib.h>
+#include <string.h>
 #include <chrono> // for time measurement
 
 #include "shader.h"
@@ -36,128 +42,147 @@
 #include <complex>
 
 
-//template<class T> // alias template for pinned allocator
-//using ThurstHostVec = std::vector<T, thrust::system::cuda::experimental::pinned_allocator<T>>;
+
+
+template<class T> // alias template for pinned allocator
+using ThurstHostVec = std::vector<T, thrust::system::cuda::experimental::pinned_allocator<T>>;
+
+
+//struct D_Joint {
+//	left
+// };
+
+struct StdJoint {
+	std::vector<int> left;// the indices of the left points
+	std::vector<int> right;// the indices of the right points
+	std::vector<int> anchor;// the 2 indices of the anchor points: left_anchor_id,right_anchor_id
+	MSGPACK_DEFINE(left, right, anchor);
+};
+class Model {
+public:
+	std::vector<std::vector<double> > vertices;// the mass xyzs
+	std::vector<std::vector<int> > edges;//the spring ids
+	std::vector<int> idVertices;// the edge id of the vertices
+	std::vector<int> idEdges;// the edge id of the springs
+	std::vector<std::vector<double> > colors;// the mass xyzs
+	std::vector<StdJoint> Joints;// the joints
+	MSGPACK_DEFINE(vertices, edges, idVertices, idEdges, colors, Joints); // write the member variables that you want to pack
+
+	Model() {
+	}
+
+	Model(const char* file_path) {
+		// get the msgpack robot model
+		// Deserialize the serialized data
+		std::ifstream ifs(file_path, std::ifstream::in | std::ifstream::binary);
+		std::stringstream buffer;
+		buffer << ifs.rdbuf();
+		msgpack::unpacked upd;//unpacked data
+		msgpack::unpack(upd, buffer.str().data(), buffer.str().size());
+		//    std::cout << upd.get() << std::endl;
+		upd.get().convert(*this);
+	}
+};
+// bot.idVertices: body,leg0,leg1,leg2,leg3,anchor0,anchor1,anchor2,anchor3,\
+					oxyz_body,oxyz_joint0_body,oxyz_joint0_leg0,oxyz_joint1_body,oxyz_joint1_leg1,\
+							  oxyz_joint2_body,oxyz_joint2_leg2,oxyz_joint3_body,oxyz_joint3_leg3,the end
+// bot.idEdges: body, leg0, leg1, leg2, leg3, anchors, rotsprings, fricsprings, oxyz_self_springs, oxyz_anchor_springs, the end
+
+//__global__ void dummykernel(MASS d_mass,int num_mass) {
+//	int i = blockDim.x * blockIdx.x + threadIdx.x; // todo: change to grid-strided loop
+//	if (i < num_mass) {
+//		d_mass.vel[i][0] = (double)i; // todo check if this is needed
+//		printf("%d", i);
+//	}
+//}
 
 
 int main()
 {
+	
 	auto start = std::chrono::steady_clock::now();
 
-	const int num_body = 5;//number of bodies
-	Model bot("..\\src\\data.msgpack"); //defined in sim.h
+	const int num_body = 5;//number of bodies, four legs and one main body
+	Model bot("..\\src\\data.msgpack");
 
-	int num_mass = bot.vertices.size(); // number of mass
-	int num_spring = bot.edges.size(); // number of spring
+	int num_mass = bot.vertices.size();
+	int num_spring = bot.edges.size();
 
-	Simulation sim(num_mass, num_spring); // Simulation object
-	MASS& mass = sim.mass; // reference variable for sim.mass
-	SPRING& spring = sim.spring; // reference variable for sim.spring
 
-	sim.global_acc = Vec(0, 0, -9.8); // global acceleration
-	sim.dt = 4e-5; // timestep
+	Simulation sim(num_mass, num_spring);
+	MASS& mass = sim.mass;
+	SPRING& spring = sim.spring;
 
-	double m = 8e-4;// mass per vertex
+	sim.global_acc = Vec(0, 0, -9.8);
+	sim.dt = 4e-5;
 
-	double spring_constant = 6e2; //spring constant for silicone leg
-	double spring_damping = 0.4; // damping for spring
+	double m = 1e-3;// mass per vertex
+	double spring_constant = 6e2;
+	double spring_damping = 0.4;
 
-	double scale_high = 2.5;// scaling factor
-	double scale_low = 0.2;
-
-	double spring_constant_high = spring_constant* scale_high;//spring constant for rigid spring
-	double spring_constant_low = spring_constant* scale_low;// spring constant for resetable spring
 
 	printf("total mass:%.2f kg\n", m * num_mass);
-
 #pragma omp parallel for
 	for (int i = 0; i < num_mass; i++)
 	{
-		mass.pos[i]= bot.vertices[i]; // position (Vec) [m]
-		mass.color[i]= bot.colors[i]; // color (Vec) [0.0-1.0]
+		mass.pos[i]= bot.vertices[i];
+		mass.color[i]= bot.colors[i];
 		mass.m[i] = m; // mass [kg]
-		mass.constrain[i] = bot.isSurface[i];// set constrain to true for suface points, and false otherwise
 	}
 #pragma omp parallel for
 	for (int i = 0; i < num_spring; i++)
 	{
-		spring.left[i] = bot.edges[i][0]; // the left mass index of the spring
-		spring.right[i] = bot.edges[i][1]; // the right mass index of the spring
+		spring.left[i] = bot.edges[i][0];
+		spring.right[i] = bot.edges[i][1];
 		spring.k[i] = spring_constant; // spring constant
-		spring.damping[i] = spring_damping; // spring constant
-		spring.rest[i] = (mass.pos[spring.left[i]] - mass.pos[spring.right[i]]).norm(); // spring rest length
-		spring.resetable[i] = false; // set all spring as non-resetable
+		spring.damping[i] = spring_damping;
+		spring.rest[i] = (mass.pos[spring.left[i]] - mass.pos[spring.right[i]]).norm();
 	}
 
-	// bot.idVertices: body,leg0,leg1,leg2,leg3,anchor0,anchor1,anchor2,anchor3,\
-					oxyz_body,oxyz_joint0_body,oxyz_joint0_leg0,oxyz_joint1_body,oxyz_joint1_leg1,\
-							  oxyz_joint2_body,oxyz_joint2_leg2,oxyz_joint3_body,oxyz_joint3_leg3,the end
-	// bot.idEdges: body, leg0, leg1, leg2, leg3, anchors, rotsprings, fricsprings, oxyz_self_springs, oxyz_anchor_springs, the end
 
 	// set higher spring constant for the robot body
 	for (int i = 0; i < bot.idEdges[1]; i++)
 	{
-		spring.k[i] = spring_constant_high;
+		spring.k[i] = 2e3;
 	}
 	// set higher spring constant for the rotational joints
-	for (int i = bot.idEdges[num_body]; i < bot.idEdges[num_body +1]; i++)
+	for (int i = bot.idEdges[num_body]; i < bot.idEdges[num_body +sim.num_joint]; i++)
 	{
-		spring.k[i] = spring_constant_high; // joints anchors
+		spring.k[i] = 2e3;
 	}
-	for (int i = bot.idEdges[num_body +1]; i < bot.idEdges[num_body +2]; i++)
-	{
-		spring.k[i] = spring_constant_high; // joints rotation spring
-	}
-
-	sim.id_restable_spring_start = bot.idEdges[num_body + 2]; // resetable spring (frictional spring)
-	sim.id_resetable_spring_end = bot.idEdges[num_body + 3];
+	sim.id_restable_spring_start = bot.idEdges[num_body + 2*sim.num_joint]; // resetable spring
+	sim.id_resetable_spring_end = bot.edges.size();
 	for (int i = sim.id_restable_spring_start; i < sim.id_resetable_spring_end; i++)
 	{
-		spring.k[i] = spring_constant_low;// resetable spring, reset the rest length per dynamic update
-		spring.damping[i] = spring_damping*1.5;
-		spring.resetable[i] = true;
+		spring.k[i] = 2e2;// resetable spring
+		spring.damping[i] = spring_damping*2.;
 	}
 
 
-	
-
-	//// the start (inclusive) and end (exclusive) index of the anchor points
-	//double id_joint_anchor_start = bot.idVertices[num_body];
-	//double id_joint_anchor_end = bot.idVertices[num_body + sim.num_joint];
-
-	//oxyz_body,oxyz_joint0_body,oxyz_joint0_leg0,oxyz_joint1_body,oxyz_joint1_leg1,\
-				oxyz_joint2_body,oxyz_joint2_leg2,oxyz_joint3_body,oxyz_joint3_leg3,
-	sim.id_oxyz_start = bot.idVertices[num_body + sim.num_joint];
-	sim.id_oxyz_end = bot.idVertices[num_body + sim.num_joint + 1 + 2* sim.num_joint];
-
-	double scale_down = 0.1;
-	// set lower mass for the anchored coordinate systems
-	for (int i = sim.id_oxyz_start; i < sim.id_oxyz_end; i++)
+#pragma omp parallel for
+	for (int i = 0; i < sim.num_joint; i++)
 	{
-		mass.m[i] = m * scale_down; // mass [kg]
+		auto& std_joint = bot.Joints[i];
+		auto& joint = sim.joints[i];
+		auto& d_joint = sim.d_joints[i];
+		joint.init(std_joint.left.size(), std_joint.right.size(), true);//init the host
+		d_joint.init(std_joint.left.size(), std_joint.right.size(), false);//init the device, TODO
+		for (int k = 0; k < std_joint.left.size(); k++)
+		{ // note that #left may not equal # right
+			joint.left[k] = std_joint.left[k];
+		}
+		for (int k = 0; k < std_joint.right.size(); k++)
+		{
+			joint.right[k] = std_joint.right[k];
+		}
+		joint.anchor[0] = std_joint.anchor[0];
+		joint.anchor[1] = std_joint.anchor[1];
 	}
-
-	for (int i = bot.idEdges[num_body + 3]; i < bot.idEdges[num_body + 4]; i++)
-	{
-		spring.k[i] = spring_constant * 2*scale_down;// oxyz_self_springs
-		spring.damping[i] = spring_damping * scale_down;
-	}
-	for (int i = bot.idEdges[num_body + 4]; i < bot.idEdges[num_body + 5]; i++)
-	{
-		spring.k[i] = spring_constant * scale_down;// oxyz_anchor_springs
-		spring.damping[i] = spring_damping * scale_down;
-	}
-
-
-	sim.joints.init(bot.Joints, true);
-	sim.d_joints.init(bot.Joints, false);
-	sim.d_joints.copyFrom(sim.joints);
-
 
 	// set max speed for each joint
 	sim.max_joint_speed = 200. / 60. * 2 * 3.1415926 * sim.dt;//200 rpm
 
-	sim.setViewport(Vec(-0.3, 0, 0.3), Vec(0, 0, 0), Vec(0, 0, 1));
+	sim.setViewport(Vec(0, -0.3, 0.3), Vec(0, 0, 0), Vec(0, 0, 1));
 	//sim.setViewport(Vec(.4, -0., .4), Vec(0, -0., -0), Vec(0, 0, 1));
 
 	// our plane has a unit normal in the z-direction, with 0 offset.
@@ -168,6 +193,12 @@ int main()
 	//double runtime = 120;
 	//sim.setBreakpoint(runtime);
 	
+	//********************************************//
+	//********************************************//
+	sim.UDP();
+	//********************************************//
+	//********************************************//
+
 
 	sim.start();
 
@@ -177,9 +208,19 @@ int main()
 		std::this_thread::sleep_for(std::chrono::milliseconds(1));
 	}
 
+	//auto end = std::chrono::steady_clock::now();
+	//std::cout << "Elapsed time:"
+	//	<< std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count()
+	//	<< " ms" << std::endl;
+
+
 	auto end = std::chrono::steady_clock::now();
 	printf("main():Elapsed time:%d ms \n",
 		std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count());
+
+
+
+	//std::complex<double> a;
 
 	return 0;
 }
