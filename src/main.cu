a3768c1e33edebb0ac060c43daadc30126ebe2d6
Boxi Xia <bx2150@columbia.edu>
2021-01-21 04:38:41 -0500

    make main more general

--- src/main.cu
+++ src/main.cu
@@ -47,16 +47,16 @@
 
 int main()
 {
-	
+
 
 	// m_total->3113g
 	// for time measurement
 	auto start = std::chrono::steady_clock::now();
 
-	
+
 	Model bot("..\\src\\data.msgpack"); //defined in sim.h
 
-	const size_t num_body = bot.idVertices.size()-3;//number of bodies
+	const size_t num_body = bot.idVertices.size() - 3;//number of bodies
 	const size_t num_mass = bot.vertices.size(); // number of mass
 	const size_t num_spring = bot.edges.size(); // number of spring
 
@@ -65,12 +65,12 @@ int main()
 	Simulation sim(num_mass, num_spring); // Simulation object
 	MASS& mass = sim.mass; // reference variable for sim.mass
 	SPRING& spring = sim.spring; // reference variable for sim.spring
-	
-	
+
+
 	//sim.dt = 4e-5; // timestep
 	sim.dt = 5e-5; // timestep
 
-	constexpr  double radius_poisson = 10*1e-3;
+	constexpr  double radius_poisson = 10 * 1e-3;
 
 	const double radius_knn = radius_poisson * sqrt(3.0);
 	constexpr double mimimun_radius = radius_poisson * 0.5;
@@ -78,7 +78,7 @@ int main()
 	const double m = 6e-4;// mass per vertex
 	//const double m = 2.5/(double)num_mass;// mass per vertex
 
-	const double spring_constant =m*2.4e6; //spring constant for silicone leg
+	const double spring_constant = m * 2.4e6; //spring constant for silicone leg
 	//const double spring_damping = m*1.8e2; // damping for spring
 	const double spring_damping = m * 1.5e2; // damping for spring
 
@@ -90,25 +90,25 @@ int main()
 	//const double scale_low = 0.5; // scaling factor low
 	constexpr double scale_probe = 0.08; // scaling factor for the probing points, e.g. coordinates
 
-	const double spring_constant_rigid = spring_constant* scale_high;//spring constant for rigid spring
+	const double spring_constant_rigid = spring_constant * scale_high;//spring constant for rigid spring
 
 	const double spring_constant_restable = spring_constant * scale_high; // spring constant for resetable spring
-	const double spring_damping_restable = spring_damping*2.4; // spring damping for resetable spring
+	const double spring_damping_restable = spring_damping * 2.4; // spring damping for resetable spring
 
 	//const double spring_constant_restable = 0; // spring constant for resetable spring
 	//const double spring_damping_restable = 0; // spring damping for resetable spring
 
 	// spring coefficient for the probing springs, e.g. coordinates
 	const double spring_constant_probe_anchor = spring_constant * scale_probe; // spring constant for coordiates anchor springs
-	const double spring_constant_probe_self = spring_constant * scale_probe* scale_high; // spring constant for coordiates self springs
+	const double spring_constant_probe_self = spring_constant * scale_probe * scale_high; // spring constant for coordiates self springs
 	const double spring_damping_probe = spring_damping * scale_probe * scale_high;
 
-	
+
 #pragma omp parallel for simd
 	for (int i = 0; i < num_mass; i++)
 	{
-		mass.pos[i]= bot.vertices[i]; // position (Vec3d) [m]
-		mass.color[i]= bot.colors[i]; // color (Vec3d) [0.0-1.0]
+		mass.pos[i] = bot.vertices[i]; // position (Vec3d) [m]
+		mass.color[i] = bot.colors[i]; // color (Vec3d) [0.0-1.0]
 		mass.m[i] = m; // mass [kg]
 		mass.constrain[i] = bot.isSurface[i];// set constraint to true for suface points, and false otherwise
 	}
@@ -125,35 +125,35 @@ int main()
 	}
 
 
-/*bot.idVertices: body,leg0,leg1,leg2,leg3,anchor,coord,the end
- bot.idEdges: body, leg0, leg1, leg2, leg3, anchors, rotsprings, fricsprings, oxyz_self_springs, oxyz_anchor_springs, the end */
-	
-	// set higher mass value for robot body
-	for (int i = bot.idVertices[0]; i < bot.idVertices[1]; i++)
-	{
-		mass.m[i] = m*1.8; // accounting for addional mass for electornics
-	}
-	// set lower mass value for leg
-	for (int i = bot.idVertices[1]; i < bot.idVertices[1+4]; i++)
-	{
-		mass.m[i] = m * 0.3; // 80% infill,no skin
-	}
+	/*bot.idVertices: body,leg0,leg1,leg2,leg3,anchor,coord,the end
+	 bot.idEdges: body, leg0, leg1, leg2, leg3, anchors, rotsprings, fricsprings, oxyz_self_springs, oxyz_anchor_springs, the end */
 
-	// set the mass value for joint
-//#pragma omp parallel for
-	
+	 //// set higher mass value for robot body
+	 //for (int i = bot.idVertices[0]; i < bot.idVertices[1]; i++)
+	 //{
+	 //	mass.m[i] = m*1.8; // accounting for addional mass for electornics
+	 //}
+	 //// set lower mass value for leg
+	 //for (int i = bot.idVertices[1]; i < bot.idVertices[num_body]; i++)
+	 //{
+	 //	mass.m[i] = m * 0.3; // 80% infill,no skin
+	 //}
+
+	 // set the mass value for joint
+ //#pragma omp parallel for
+
 	for (int i = 0; i < bot.Joints.size(); i++)
 	{
 		for each (int j in bot.Joints[i].left)
 		{
-			mass.m[j] = m*1.4;
-		}	
+			mass.m[j] = m * 1.4;
+		}
 		for each (int j in bot.Joints[i].right)
 		{
-			mass.m[j] = m*1.4;
+			mass.m[j] = m * 1.4;
 		}
 	}
-	
+
 
 	// set higher spring constant for the robot body
 	for (int i = 0; i < bot.idEdges[1]; i++)
@@ -162,12 +162,12 @@ int main()
 		spring.k[i] *= scale_high;
 	}
 	// set higher spring constant for the rotational joints
-	for (int i = bot.idEdges[num_body]; i < bot.idEdges[num_body +1]; i++)
+	for (int i = bot.idEdges[num_body]; i < bot.idEdges[num_body + 1]; i++)
 	{
 		spring.k[i] = spring_constant_rigid; // joints anchors
 		//spring.k[i] *= scale_high;
 	}
-	for (int i = bot.idEdges[num_body +1]; i < bot.idEdges[num_body +2]; i++)
+	for (int i = bot.idEdges[num_body + 1]; i < bot.idEdges[num_body + 2]; i++)
 	{
 		spring.k[i] = spring_constant_rigid; // joints rotation spring
 		//spring.k[i] *= scale_high;
@@ -183,15 +183,12 @@ int main()
 		spring.resetable[i] = true;
 	}
 
-	//// the start (inclusive) and end (exclusive) index of the anchor points
-	//double id_joint_anchor_start = bot.idVertices[num_body];
-	//double id_joint_anchor_end = bot.idVertices[num_body + num_joint];
 
 	/*oxyz_body,oxyz_joint0_body,oxyz_joint0_leg0,oxyz_joint1_body,oxyz_joint1_leg1,
 				oxyz_joint2_body,oxyz_joint2_leg2,oxyz_joint3_body,oxyz_joint3_leg3,*/
 	sim.id_oxyz_start = bot.idVertices[num_body + 1];
 	sim.id_oxyz_end = bot.idVertices[num_body + 2];
-	
+
 	// set lower mass for the anchored coordinate systems
 	for (int i = sim.id_oxyz_start; i < sim.id_oxyz_end; i++)
 	{
@@ -213,32 +210,34 @@ int main()
 	sim.d_joint.init(bot.Joints, false);
 	sim.d_joint.copyFrom(sim.joint);
 
-	
 
 
 
+	// print out mass statistics
 	double total_mass = 0;
 #pragma omp parallel for shared (total_mass,mass.m) reduction(+:total_mass)
-	for (int i = 0; i < num_mass; i++){total_mass += mass.m[i];}
+	for (int i = 0; i < num_mass; i++) { total_mass += mass.m[i]; }
+	printf("total mass:%.2f [kg]\n",total_mass);
 
-	double body_mass = 0;
-	for (int i = bot.idVertices[0]; i < bot.idVertices[1]; i++)
-	{body_mass+= mass.m[i];}// calculate body mass
-	
+	std::vector<double> part_mass(num_body,0); // vector of size num_body, with values 0
+#pragma omp parallel for
+	for (int k = 0; k < num_body; k++)
+		for (int i = bot.idVertices[k]; i < bot.idVertices[k + 1]; i++)
+			part_mass[k] += mass.m[i];
+	printf("part mass:");
+	for (const auto& i : part_mass) 
+		printf("%.2f ", i);
+	printf("[kg]\n");
 
-	double leg_mass = 0;
-	for (int i = bot.idVertices[1]; i < bot.idVertices[2]; i++)
-	{leg_mass += mass.m[i];}// calculate leg mass
-
-	double joint_mass = 0;
-	for each (int j in bot.Joints[0].right)
-	{joint_mass+=mass.m[j];}// calculate joint mass
-
-
-
-	printf("total mass:%.2f kg, body mass:%.2f kg, per leg mass:%.2f kg (soft part:%.2f kg)\n", 
-		total_mass, body_mass, leg_mass, leg_mass - joint_mass);
-
+	std::vector<double> joint_mass(num_joint, 0); // vector of size num_body, with values 0
+#pragma omp parallel for
+	for (int k = 0; k < num_joint; k++)
+		for (const auto & i:bot.Joints[k].left)
+			joint_mass[k] += mass.m[i];
+	printf("joint mass:");
+	for (const auto& i : joint_mass)
+		printf("%.2f ", i);
+	printf("[kg]\n");
 
 
 	sim.jc.init(num_joint, true);//init joint controller
@@ -253,9 +252,6 @@ int main()
 	//sim.setViewport(Vec3d(0.6, 0, 0.3), Vec3d(0, 0, 0.2), Vec3d(0, 0, 1));
 	sim.setViewport(Vec3d(1.75, -2.5, 1.0), Vec3d(1.75, 0, 0.1), Vec3d(0, 0, 1));
 #endif // GRAPHICS
-
-
-
 
 	// our plane has a unit normal in the z-direction, with 0 offset.
 	//sim.createPlane(Vec3d(0, 0, 1), -1, 0, 0);
